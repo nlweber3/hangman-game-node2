c314ceb0b7285701e5b19c303ea4c1b0cf45fb5c
Nick <nlweber3@gmail.com>
2018-02-18 22:59:57 -0800

    4g test 5

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var inquirer = require("inquirer");
 
-// Create a "Prompt" with a series of questions.
+
+
 inquirer
   .prompt([
-    // Here we create a basic text prompt.
+
     {
       type: "input",
       message: "GUESS A LETTER",
